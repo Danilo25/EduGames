80b4f3db30b3c0d2b601579cb2d3d23fdca92edf
Danilo <danilo25aciole@gmail.com>
2019-01-30 10:27:12 -0200

    atualização

---
+++ QC/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1548850977,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/logo1-sheet0.png",
+		"images/logo2-sheet0.png",
+		"images/peças-sheet0.png",
+		"images/montar-sheet0.png",
+		"images/montar-sheet1.png",
+		"images/play-sheet0.png",
+		"images/falsocarregamento-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
